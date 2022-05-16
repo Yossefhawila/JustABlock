698c64002b203bf3479ccf720abc38741041cca9
Yossefhawila <92972339+Yossefhawila@users.noreply.github.com>
2022-05-16 16:41:08 +0200

    MenuManagerAdded

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    int currentMenu = 1;
+    [SerializeField]
+    private GameObject Mainmenu;
+    [SerializeField]
+    private GameObject ShopMenu;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        MenuAutoChangerByClick();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void SelectMenu(int menuIndex)
+    {
+        if(menuIndex == 0)
+        {
+            Mainmenu.SetActive(true);
+            ShopMenu.SetActive(false);
+            currentMenu = 0;
+        }
+        else
+        {
+            ShopMenu.SetActive(true);
+            Mainmenu.SetActive(false);
+            currentMenu = 1;
+        }
+    }
+    public void MenuAutoChangerByClick()
+    {
+        if (currentMenu == 0)
+        {
+            SelectMenu(1);
+        }
+        else
+        {
+            SelectMenu(0);
+        }
+    }
+}
