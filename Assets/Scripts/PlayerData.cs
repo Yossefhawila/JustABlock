a1e0ebb234b660096d93f08bfbfb33bc85aa7915
Yossefhawila <92972339+Yossefhawila@users.noreply.github.com>
2022-05-17 17:07:59 +0200

    SaveGameDataAdded

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using UnityEngine.UI;
+using System.IO;
 
 public class PlayerData : MonoBehaviour
 
@@ -19,6 +20,7 @@ public class PlayerData : MonoBehaviour
     public double BuyPriceMultiplierUpgrade=1000;
     public double ClickMultiplier;
     public double ClickMultiplierupgrade=100;
+
 
     private double _playerMoneyValue=0;
     public double PlayerMoneyValue 
@@ -46,6 +48,7 @@ public class PlayerData : MonoBehaviour
     
     public void ADDMoney(double Amount)
     {
+        SaveData();
         PlayerMoneyValue += Amount;
         PlayerMoneyTextInUi.text = getMoneyText(PlayerMoneyValue);
     }
@@ -93,9 +96,69 @@ public class PlayerData : MonoBehaviour
 
     private void Awake()
     {
+        if (File.Exists(Application.persistentDataPath + "/jsonData.json"))
+        {
+            LoadData();
+        }
         instance = this;
-        PlayerMoneyTextInUi = gameObject.GetComponent<Text>();  
+        PlayerMoneyTextInUi = gameObject.GetComponent<Text>();
+        PlayerMoneyTextInUi.text = getMoneyText(PlayerMoneyValue);
     }
+
+    [SerializeField]
+    class PlayerGameData 
+    {
+        public double MaterialValue;
+        public double MaterialValueUpgrade = 20000;
+        public double SellPrice;
+        public double SellMultiplier;
+        public double SellMultiplierUpgrade = 500;
+        public double BuyPrice;
+        public double BuyPriceMultiplier;
+        public double BuyPriceMultiplierUpgrade = 1000;
+        public double ClickMultiplier;
+        public double ClickMultiplierupgrade = 100;
+        public double PlayerMoney;
+    }
+    private void SaveData()
+    {
+        PlayerGameData data = new PlayerGameData();
+        data.MaterialValue = MaterialValue;
+        data.MaterialValueUpgrade = MaterialValueUpgrade;
+        data.SellPrice = SellPrice;
+        data.SellMultiplier = SellMultiplier;
+        data.SellMultiplierUpgrade = SellMultiplierUpgrade;
+        data.BuyPrice = BuyPrice;
+        data.BuyPriceMultiplier = BuyPriceMultiplier;
+        data.BuyPriceMultiplierUpgrade = BuyPriceMultiplierUpgrade;
+        data.ClickMultiplier = ClickMultiplier;
+        data.ClickMultiplierupgrade = ClickMultiplierupgrade;
+        data.PlayerMoney = PlayerMoneyValue;
+        
+        string JsonData = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/jsonData.json", JsonData);
+
+    }
+    
+    private void LoadData()
+    {
+        PlayerGameData data = new PlayerGameData();
+        string json = File.ReadAllText(Application.persistentDataPath + "/jsonData.json");
+        data = JsonUtility.FromJson<PlayerGameData>(json);
+        MaterialValue = data.MaterialValue;
+        MaterialValueUpgrade = data.MaterialValueUpgrade;
+        SellPrice = data.SellPrice;
+        SellMultiplier = data.SellMultiplier;
+        SellMultiplierUpgrade= data.SellMultiplierUpgrade;
+        BuyPrice = data.BuyPrice;
+        BuyPriceMultiplier= data.BuyPriceMultiplier;
+        BuyPriceMultiplierUpgrade = data.BuyPriceMultiplierUpgrade;
+        ClickMultiplier = data.ClickMultiplier;
+        ClickMultiplierupgrade = data.ClickMultiplierupgrade;
+        PlayerMoneyValue = data.PlayerMoney;
+
+    }
+
 
 
 }
